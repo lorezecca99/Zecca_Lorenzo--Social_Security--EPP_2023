e50a55fbfa61a03be30c6bcea8789aa515a61b6f
lorezecca99 <115668250+lorezecca99@users.noreply.github.com>
2023-03-29 23:53:44 +0200

    Import items from template

---
+++ src/ss_us/__init__.py
@@ -0,0 +1,1 @@
+"""Code for producing the results of the Macroeconomic consequences of eliminating social security in the US project."""
---
+++ src/ss_us/data_management/__init__.py
@@ -0,0 +1,5 @@
+"""Functions for managing data."""
+
+from ss_us.data_management.clean_data import clean_data
+
+__all__ = [clean_data]
